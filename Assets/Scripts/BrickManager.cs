d872948262ca129e3daaf9e0a422b4d0673a7c46
Stefan Büttner <buettner.stefan@web.de>
2017-03-17 19:32:55 +0100

    Brick managgr can adjust sizc.

--- Assets/Scripts/BrickManager.cs
+++ Assets/Scripts/BrickManager.cs
@@ -8,11 +8,13 @@ public class BrickManager : MonoBehaviour
 
 	public GameObject brickPrefab;
 
+	[Range(1, 25)]
 	public int xCount = 17;
+	[Range(1, 20)]
 	public int yCount = 8;
 
-	public int currXCount = 0;
-	public int currYCount = 0;
+	private int currXCount = 0;
+	private int currYCount = 0;
 
 
 	void Start()
@@ -35,16 +37,14 @@ public class BrickManager : MonoBehaviour
 
 		if (currXCount < xCount)
 		{
-			for (int x = currXCount; x < xCount; ++x)
+			for (int y = 0; y < currYCount; ++y)
 			{
-				for (int y = 0; y < currYCount; ++y)
+				for (int x = currXCount; x < xCount; ++x)
 				{
 					GameObject newBrick = GameObject.Instantiate(brickPrefab,
-																 GetBrickPosition(x, y, xCount, currYCount),
-																 Quaternion.identity,
 																 gameObject.transform);
 					// Important to use the new xCount!
-					m_Bricks.Insert(y * xCount + x, newBrick);
+					m_Bricks.Insert(y * xCount + currXCount, newBrick);
 				}
 			}
 		}
@@ -61,6 +61,7 @@ public class BrickManager : MonoBehaviour
 		}
 
 		currXCount = xCount;
+		RecalculatePositions();
 	}
 
 	void UpdateY()
@@ -75,8 +76,6 @@ public class BrickManager : MonoBehaviour
 				for (int x = 0; x < currXCount; ++x)
 				{
 					GameObject newBrick = GameObject.Instantiate(brickPrefab,
-																 GetBrickPosition(x, y, currXCount, yCount),
-																 Quaternion.identity,
 																 gameObject.transform);
 					m_Bricks.Add(newBrick);
 				}
@@ -84,7 +83,7 @@ public class BrickManager : MonoBehaviour
 		}
 		else if (yCount < currYCount)
 		{
-			for (int y = 0; y < currYCount - yCount; ++y)
+			for (int y = yCount; y < currYCount; ++y)
 			{
 				for (int x = 0; x < currXCount; ++x)
 				{
@@ -95,6 +94,18 @@ public class BrickManager : MonoBehaviour
 		}
 
 		currYCount = yCount;
+		RecalculatePositions();
+	}
+
+	private void RecalculatePositions()
+	{
+		for (int y = 0; y < currYCount; ++y)
+		{
+			for (int x = 0; x < currXCount; ++x)
+			{
+				m_Bricks[x + y * currXCount].transform.position = GetBrickPosition(x, y, currXCount, currYCount);
+			}
+		}
 	}
 
 	private Vector3 GetBrickPosition(int x, int y, int width, int height)
@@ -102,7 +113,10 @@ public class BrickManager : MonoBehaviour
 		if (brickPrefab == null)
 			return Vector3.zero;
 
-		return new Vector3(x * brickPrefab.transform.localScale.x, y * brickPrefab.transform.localScale.y, 0);
+		float xPos = (x - (width / 2f)) * brickPrefab.transform.localScale.x;
+		float yPos = (y - (height / 2f)) * brickPrefab.transform.localScale.y;
+
+		return new Vector3(xPos, yPos, 0);
 	}
 
 	void OnDestroy()
