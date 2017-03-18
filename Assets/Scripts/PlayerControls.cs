cea65ba56f97a829c0e67add25b41ed282256be6
Stefan Büttner <buettner.stefan@web.de>
2017-03-18 18:40:27 +0100

    Cleanup PlayerControls.

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,20 +1,11 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class PlayerControls : MonoBehaviour {
-
-    Vector3 position;
-
-    // Use this for initialization
-    void Start () {
-        position = this.gameObject.transform.position;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
+public class PlayerControls : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
         float movement = Input.GetAxis("Mouse X");
-        this.gameObject.transform.Translate(movement,0,0);
+        this.gameObject.transform.Translate(movement, 0, 0);
     }
-
 }
