6b819f30a54c16b59d82475aa034a49ead6ad3b9
Stefan Büttner <buettner.stefan@web.de>
2017-03-26 21:22:06 +0200

    Add ability to quit the game in the main menu by hitting escape.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,6 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+	void Update()
+	{
+		if (Input.GetKeyUp(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+	}
+
 	public void OnPlayPressed()
 	{
 		Settings s = Settings.instance;
